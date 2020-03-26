187196c691cb0f7c309d3d9a50b9566dc2936dc5
Josh Nussbaum <joshnuss@gmail.com>
2020-03-26 03:12:01 -0400

    Adds polka/send-type middleware

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 import sirv from 'sirv'
 import polka from 'polka'
+import send from '@polka/send-type'
 import compression from 'compression'
 import * as sapper from '@sapper/server'
 
 const { PORT, NODE_ENV } = process.env
 const dev = NODE_ENV === 'development'
 
+function sendMiddleware(req, res, next) {
+  res.send = send.bind(null, res)
+  next()
+}
+
 polka() // You can also use Express
   .use(
     compression({ threshold: 0 }),
     sirv('static', { dev }),
+    sendMiddleware,
     sapper.middleware()
   )
   .listen(PORT, err => {
